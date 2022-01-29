b1447a7e779ed1851edcff82c7e176a70d892f7a
nataly <nvchulichkova@yandex.ru>
2022-01-29 22:32:27 +0300

    add scss

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,24 @@
-const page = document.querySelector(".page");
-const navPopup = page.querySelector(".pop-up-nav");
+const page = document.querySelector('.page');
+const navPopup = page.querySelector('.popup');
 
-const burgerButton = page.querySelector(".header__burger");
-const navPopUpCloseButton = page.querySelector(".pop-up-nav__close-button");
+const burgerButton = page.querySelector('.header__menu-burger');
+const navPopUpCloseButton = page.querySelector(".popup__close-button");
 
-const mailForm = page.querySelector(".form");
-const mailInput = mailForm.querySelector(".form__input");
-const mailFormButton = mailForm.querySelector(".form__btn");
+const mailForm = page.querySelector('.form');
+const mailInput = mailForm.querySelector('.form__input');
+const mailFormButton = mailForm.querySelector('.form__btn');
 
-burgerButton.addEventListener("click", function () {
-  navPopup.classList.add("pop-up-nav_opened");
+burgerButton.addEventListener('click', function () {
+  navPopup.classList.add('popup_opened');
 });
 
-navPopUpCloseButton.addEventListener("click", function () {
-  navPopup.classList.remove("pop-up-nav_opened");
+navPopUpCloseButton.addEventListener('click', function () {
+  navPopup.classList.remove('popup_opened');
 });
 
-mailFormButton.addEventListener("click", function () {
-  if (mailInput.value !== "") {
-    mailFormButton.textContent = "Спасибо!";
+mailFormButton.addEventListener('click', function () {
+  if (mailInput.value !== '') {
+    mailFormButton.textContent = 'Спасибо!';
   } else {
     return;
   }
