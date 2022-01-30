b954519a25cc1068771586660e78430b897ff98d
nataly <nvchulichkova@yandex.ru>
2022-01-30 14:00:22 +0300

    feat: добавить js

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,17 @@
 const page = document.querySelector('.page');
-const navPopup = page.querySelector('.popup');
-
+const popup = page.querySelector('.popup');
 const burgerButton = page.querySelector('.header__menu-burger');
-const navPopUpCloseButton = page.querySelector(".popup__close-button");
-
+const popupCloseButton = page.querySelector('.popup__close-button');
 const mailForm = page.querySelector('.form');
 const mailInput = mailForm.querySelector('.form__input');
 const mailFormButton = mailForm.querySelector('.form__btn');
 
 burgerButton.addEventListener('click', function () {
-  navPopup.classList.add('popup_opened');
+  popup.classList.add('popup_opened');
 });
 
-navPopUpCloseButton.addEventListener('click', function () {
-  navPopup.classList.remove('popup_opened');
+popupCloseButton.addEventListener('click', function () {
+  popup.classList.remove('popup_opened');
 });
 
 mailFormButton.addEventListener('click', function () {
